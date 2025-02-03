fb489bb132ed91eb51db9d5bcc511b86760fcf86
laytonmusyoki <laytonmatheka@gmail.com>
2025-02-03 17:35:13 +0300

    dashboard setup

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,15 @@
 <?php
 
+use App\Http\Controllers\PatientController;
+use App\Http\Controllers\StaffController;
 use Illuminate\Support\Facades\Route;
 
 //online patients
+Route::get('/user/patient',[PatientController::class,'index'])->name('user.patient');
 
+
+// staff configurations
+Route::get('/staff/admin',[StaffController::class,'index'])->name('staff.admin');
 
 
 //receptionist
